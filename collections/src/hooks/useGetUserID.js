801b439deea74422102f3e849d95494a9d1dc6b9
Jessica Lane <48732966+jesslane94@users.noreply.github.com>
2024-06-25 17:04:41 -0700

    actually delete git ignore this time? untrack files

--- collections/src/hooks/useGetUserID.js
+++
@@ -1,5 +0,0 @@
-export const useGetUserID = () => {
-    const { name, profilePhoto, userID, isAuth } = JSON.parse(
-        localStorage.getItem("auth")) || {};
-    return { name, profilePhoto, userID, isAuth };
-};
